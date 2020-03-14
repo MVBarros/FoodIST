c25d970c82d4faeff9189d208fdaeeef613a539b
Miguel Barros <miguel.v.barros@tecnico.ulisboa.pt>
2020-03-14 12:58:29 +0000

    Server running on gradle

--- Server/server/src/main/java/foodist/server/FoodServer.java
+++ Server/server/src/main/java/foodist/server/FoodServer.java
@@ -1,9 +1,22 @@
 package foodist.server;
 
+import foodist.server.service.FoodServiceImpl;
+import io.grpc.BindableService;
+import io.grpc.Server;
+import io.grpc.netty.shaded.io.grpc.netty.NettyServerBuilder;
+
+import java.io.IOException;
+
 public class FoodServer {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Hello World Server");
+
+        final BindableService service = new FoodServiceImpl();
+        Server server = NettyServerBuilder.forPort(Integer.parseInt(args[0])).addService(service).build();
+        server.start();
+
+        server.awaitTermination();
         return;
     }
 }
