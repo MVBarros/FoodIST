b12ac4dbd507a1a7725e17ecf5eabbdcc4ffb2c9
Miguel Barros <miguel.v.barros@tecnico.ulisboa.pt>
2020-03-14 18:04:38 +0000

    Improved Server Module Setup

---
+++ FoodIST-Server/server/src/main/java/foodist/server/service/ServiceImplementation.java
@@ -0,0 +1,6 @@
+package foodist.server.service;
+
+import foodist.server.grpc.contract.FoodISTServerServiceGrpc.FoodISTServerServiceImplBase;
+
+public class ServiceImplementation extends FoodISTServerServiceImplBase {
+}
