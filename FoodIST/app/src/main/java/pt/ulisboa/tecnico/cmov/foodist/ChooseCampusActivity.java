49d8844766d4f7119feaafb7022baa457904960d
JRafaelSoares <joao.rafael.pinto.soares@tecnico.ulisboa.pt>
2020-03-15 18:46:34 +0000

    Created Campus activity
    Closes #3

--- FoodIST/app/src/main/java/pt/ulisboa/tecnico/cmov/foodist/ChooseCampusActivity.java
+++ FoodIST/app/src/main/java/pt/ulisboa/tecnico/cmov/foodist/ChooseCampusActivity.java
@@ -2,13 +2,38 @@ package pt.ulisboa.tecnico.cmov.foodist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChooseCampusActivity extends AppCompatActivity {
-
+    public static final String CAMPUS = "pt.ulisboa.tecnico.cmov.foodlist.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_campus);
+
+        Button tagusparkButton = (Button) findViewById(R.id.taguspark);
+        Button alamedaButton = (Button) findViewById(R.id.alameda);
+
+        tagusparkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChooseCampusActivity.this, MainActivity.class);
+                intent.putExtra(CAMPUS,"Taguspark");
+                startActivity(intent);
+            }
+        });
+
+        alamedaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChooseCampusActivity.this, MainActivity.class);
+                intent.putExtra(CAMPUS,"Alameda");
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- FoodIST/app/src/main/java/pt/ulisboa/tecnico/cmov/foodist/MainActivity.java
+++ FoodIST/app/src/main/java/pt/ulisboa/tecnico/cmov/foodist/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +15,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Intent intent = getIntent();
+        String campus = intent.getStringExtra(ChooseCampusActivity.CAMPUS);
+
         Button userProfileButton = (Button) findViewById(R.id.userProfile);
         Button changeCampusButton = (Button) findViewById(R.id.changeCampus);
+
+        //While in testing, intent can have message null
+        if(campus != null){
+            TextView textView = (TextView) findViewById(R.id.currentCampus);
+            textView.setText(campus);
+        }
 
         userProfileButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
 
     }
 
