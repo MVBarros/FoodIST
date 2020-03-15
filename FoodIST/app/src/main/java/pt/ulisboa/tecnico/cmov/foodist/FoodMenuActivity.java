abd6306fdf2f305908ac5aa138de75f75a9d2fce
Miguel Barros <miguel.v.barros@tecnico.ulisboa.pt>
2020-03-15 21:02:31 +0000

    Add Button Functionality

--- FoodIST/app/src/main/java/pt/ulisboa/tecnico/cmov/foodist/FoodMenuActivity.java
+++ FoodIST/app/src/main/java/pt/ulisboa/tecnico/cmov/foodist/FoodMenuActivity.java
@@ -2,7 +2,10 @@ package pt.ulisboa.tecnico.cmov.foodist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FoodMenuActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class FoodMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_menu);
+
+        Button b = findViewById(R.id.add_menu_button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FoodMenuActivity.this, AddMenuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
