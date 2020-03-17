18fe3c61c16e7bf85f4252d6febc04ea829517fe
JRafaelSoares <joao.rafael.pinto.soares@tecnico.ulisboa.pt>
2020-03-17 19:16:59 +0000

    Issue #14 - Added requests for Camera/Gallery access

--- FoodIST/app/src/main/java/pt/ulisboa/tecnico/cmov/foodist/ProfileActivity.java
+++ FoodIST/app/src/main/java/pt/ulisboa/tecnico/cmov/foodist/ProfileActivity.java
@@ -1,11 +1,18 @@
 package pt.ulisboa.tecnico.cmov.foodist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -15,12 +22,28 @@ import java.io.File;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private static final int PICK_FROM_GALLERY = 1;
+    private static final int PICK_FROM_CAMERA = 2;
+    private static final int REQUEST_PIC = 3;
+
+
+    private static final String TAG = "TAG_ProfileActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
         getPreferences();
+
+        ImageView profilePicture = (ImageView) findViewById(R.id.profilePicture);
+
+        profilePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                askGalleryPermission();
+            }
+        });
     }
 
     @Override
@@ -39,6 +62,76 @@ public class ProfileActivity extends AppCompatActivity {
         editor.putInt(getString(R.string.position), status.getCheckedRadioButtonId());
 
         editor.apply();
+    }
+
+    private void askGalleryPermission() {
+        int galleryPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (galleryPermission != PackageManager.PERMISSION_GRANTED){
+            //May need to add Manifest.permission.Write_External_Storage
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PICK_FROM_GALLERY);
+        }
+        else{
+            askCameraPermission();
+        }
+    }
+
+    private void askCameraPermission() {
+        int cameraPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if (cameraPermission != PackageManager.PERMISSION_GRANTED){
+            //May need to add Manifest.permission.Write_External_Storage
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, PICK_FROM_CAMERA);
+        }
+        else{
+            cameraOrGalleryChooser();
+        }
+    }
+
+    private void cameraOrGalleryChooser(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+            Intent galleryintent = new Intent(Intent.ACTION_PICK, null);
+            galleryintent.setType("image/*");
+
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+
+                Intent chooser = new Intent(Intent.ACTION_CHOOSER);
+                chooser.putExtra(Intent.EXTRA_INTENT, galleryintent);
+                chooser.putExtra(Intent.EXTRA_TITLE, "Select from:");
+
+                Intent[] intentArray = { cameraIntent };
+                chooser.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
+                startActivityForResult(chooser, REQUEST_PIC);
+            }
+            else{
+                startActivityForResult(galleryintent, REQUEST_PIC);
+            }
+        }
+        else {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, REQUEST_PIC);
+            }
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults)
+    {
+        switch (requestCode) {
+            case PICK_FROM_GALLERY:
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "phone gallery permission granted");
+                }
+                askCameraPermission();
+                break;
+
+            case PICK_FROM_CAMERA:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "phone camera permission granted");
+                }
+                cameraOrGalleryChooser();
+                break;
+        }
     }
 
     private void getPreferences(){
