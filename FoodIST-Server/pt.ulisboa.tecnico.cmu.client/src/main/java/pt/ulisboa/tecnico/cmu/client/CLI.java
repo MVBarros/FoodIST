93019964d008e4d5f712105eddb54569d4d1bcaa
ist182069 <jose.bras@tecnico.ulisboa.pt>
2020-03-17 20:44:22 +0000

    Dummy Added

--- FoodIST-Server/pt.ulisboa.tecnico.cmu.client/src/main/java/pt/ulisboa/tecnico/cmu/client/CLI.java
+++ FoodIST-Server/pt.ulisboa.tecnico.cmu.client/src/main/java/pt/ulisboa/tecnico/cmu/client/CLI.java
@@ -2,14 +2,20 @@ package pt.ulisboa.tecnico.cmu.client;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
+import pt.ulisboa.tecnico.cmu.protocol.DummyArray;
+import pt.ulisboa.tecnico.cmu.protocol.DummySend;
+import pt.ulisboa.tecnico.cmu.protocol.DummySummary;
 import pt.ulisboa.tecnico.cmu.protocol.FoodISTServiceGrpc;
 import pt.ulisboa.tecnico.cmu.protocol.PingRequest;
 import pt.ulisboa.tecnico.cmu.protocol.PingResponse;
 
 public class CLI {
 	
+	private static final String DUMMY = "dummy";
 	private static final String PING = "ping";
 	private static final String QUIT = "quit";
 	
@@ -22,19 +28,37 @@ public class CLI {
 		while(true) {		
 			System.out.print("Enter your command to be run on the server: ");
 			String command = scanner.nextLine();	   	    
-	    
-	    	switch(command) {
-			case PING:
-		        FoodISTServiceGrpc.FoodISTServiceBlockingStub stub = FoodISTServiceGrpc.newBlockingStub(channel);        
-		        System.out.println("Ping message sent to server!");                
-		        PingResponse response = stub.ping(PingRequest.newBuilder().setPing("ping").build());        
-		        System.out.println("Response received from server: " + response.getPong());		        
-				break;
-			case QUIT:
-				channel.shutdown();
-				return;
-			default:
-				System.out.println("Error! The input command \"" + command + "does not exist!");
+			
+			FoodISTServiceGrpc.FoodISTServiceBlockingStub stub = FoodISTServiceGrpc.newBlockingStub(channel);
+	    	switch(command) {	    			    	
+		    	case DUMMY:		    		
+		    		DummyObject dummy = new DummyObject();
+		    		dummy.setDummyString("test");
+		    		dummy.setDummyArrayList();		    			    				    		      		    			    		
+		    				    				       
+		    		DummySend.Builder builder = DummySend.newBuilder();
+			        builder.setDummyString(dummy.getDummyString());			        
+			        ArrayList<String> dummyList = dummy.getDummyArrayList();
+			        			        
+			        for(int i = 0; i<dummyList.size(); i++) {
+			        	String dummyStringItem = dummyList.get(i);
+			        	DummyArray dummyArrayItem = DummyArray.newBuilder().setDummyContent(dummyStringItem).build();
+			        	builder.addArrayContent(dummyArrayItem);
+			        }			        
+			        System.out.println("Sending dummy object to server!");
+			        DummySummary dummySummary = stub.dummy(builder.build());        
+			        System.out.println("Response received from server: " + dummySummary.getSummary());		        
+			        break;			        
+				case PING:
+			        System.out.println("Ping message sent to server!");                
+			        PingResponse response = stub.ping(PingRequest.newBuilder().setPing("ping").build());        
+			        System.out.println("Response received from server: " + response.getPong());		        
+					break;					
+				case QUIT:
+					channel.shutdown();
+					return;					
+				default:
+					System.out.println("Error! The input command \"" + command + "does not exist!");
 	    	}
 	    }					
 	}
--- FoodIST-Server/pt.ulisboa.tecnico.cmu.protocol/src/main/java/pt/ulisboa/tecnico/cmu/protocol/FoodISTServiceLibrary.java
+++ FoodIST-Server/pt.ulisboa.tecnico.cmu.protocol/src/main/java/pt/ulisboa/tecnico/cmu/protocol/FoodISTServiceLibrary.java
@@ -7,4 +7,7 @@ public abstract class FoodISTServiceLibrary extends FoodISTServiceImplBase {
 	
     @Override
     public abstract void ping(PingRequest request, StreamObserver<PingResponse> responseObserver);
+    
+    @Override
+    public abstract void dummy(DummySend dummy, StreamObserver<DummySummary> responseObserver);
 }
