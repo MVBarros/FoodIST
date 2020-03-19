226d99e41e01ded9402d01fafd27de8c0e19d967
José Brás <jose.bras@tecnico.ulisboa.pt>
2020-03-19 20:32:58 +0000

    cleaning up trash

--- FoodIST-Server/client_example/src/main/java/pt/ulisboa/tecnico/cmov/client/Main.java
+++
@@ -1,8 +0,0 @@
-package pt.ulisboa.tecnico.cmov.client;
-
-public class Main {
-
-	public static void main(String[] args) {				
-		new CLI().execute();
-    }
-}
--- FoodIST-Server/server/src/main/java/pt/ulisboa/tecnico/cmov/server/Main.java
+++
@@ -1,8 +0,0 @@
-package pt.ulisboa.tecnico.cmov.server;
-
-public class Main {
-	
-	public static void main(String[] args)  {	
-		new GrpcImpl().execute();        
-    }
-}
