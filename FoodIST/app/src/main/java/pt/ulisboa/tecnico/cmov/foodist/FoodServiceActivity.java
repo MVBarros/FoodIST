4223aa62dc4d29b56060f241507dc90a8a80bbcb
JRafaelSoares <joao.rafael.pinto.soares@tecnico.ulisboa.pt>
2020-03-21 18:40:24 +0000

    Made Menus dynamic and changed way to dynamic food services

--- FoodIST/app/src/main/java/pt/ulisboa/tecnico/cmov/foodist/FoodServiceActivity.java
+++ FoodIST/app/src/main/java/pt/ulisboa/tecnico/cmov/foodist/FoodServiceActivity.java
@@ -1,12 +1,19 @@
 package pt.ulisboa.tecnico.cmov.foodist;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class FoodServiceActivity extends AppCompatActivity {
 
@@ -22,16 +29,34 @@ public class FoodServiceActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String serviceName = intent.getStringExtra("Service Name");
         foodServiceName.setText(serviceName);
+        //TODO - Ask server for list of Menus
+        ArrayList<String[]> menus = new ArrayList<>();
+        menus.add(new String[]{"Tosta mista", "10€"});
 
-        //TODO - Add menus dinamically
-        LinearLayout menu0 = (LinearLayout) findViewById(R.id.menu0);
+        for(String[] menu : menus){
 
-        menu0.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(FoodServiceActivity.this, FoodMenuActivity.class);
-                startActivity(intent);
-            }
-        });
+            //number of info
+            LayoutInflater vi = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View v = vi.inflate(R.layout.food_menu, null);
+
+            TextView name = v.findViewById(R.id.menuFood);
+            TextView cost = v.findViewById(R.id.menuCost);
+
+
+            name.setText(menu[0]);
+            cost.setText(menu[1]);
+
+            v.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(FoodServiceActivity.this, FoodMenuActivity.class);
+                    startActivity(intent);
+                }
+            });
+
+            ViewGroup foodServiceList = findViewById(R.id.menus);
+            foodServiceList.addView(v);
+
+        }
     }
 }
