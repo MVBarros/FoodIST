798c63c55fb4c3ff22a7567fe8dcd597c5cb0080
miguel <miguel.v.barros@tecnico.ulisboa.pt>
2020-04-16 13:48:13 +0100

    Server SSH

--- FoodIST-Server/server/src/main/java/foodist/server/Main.java
+++ FoodIST-Server/server/src/main/java/foodist/server/Main.java
@@ -6,7 +6,17 @@ import io.grpc.ServerBuilder;
 import foodist.server.service.ServiceImplementation;
 import foodist.server.thread.Cleanup;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.time.Duration;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -15,16 +25,22 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws Exception {
         if (args.length < 1) {
             System.out.println("Usage: [prog] port");
             return;
         }
+        File priv = getPriv();
+        File cert = getPub();
         
         final BindableService bindableService = new ServiceImplementation();
         int port = Integer.parseInt(args[0]);
         
-        Server server = ServerBuilder.forPort(port).addService(bindableService).build();
+        Server server = ServerBuilder
+                .forPort(port)
+                .useTransportSecurity(cert, priv)
+                .addService(bindableService)
+                .build();
 
         System.out.println("Server Starting");
         server.start();
@@ -45,4 +61,20 @@ public class Main {
         System.out.println("Server Started at port " + port);
         server.awaitTermination();
     }
+
+    public static File getPub() throws Exception {
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        URL url = classloader.getResource("server.cert");
+        Path path = Paths.get(url.toURI());
+        return path.toFile();
+    }
+
+
+    public static File getPriv() throws Exception {
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        URL url = classloader.getResource("server.key");
+        Path path = Paths.get(url.toURI());
+        return path.toFile();
+    }
+
 }
