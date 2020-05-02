38e97ce94dedb39fdf6c428042d599140ab07662
JRafaelSoares <joao.rafael.pinto.soares@tecnico.ulisboa.pt>
2020-05-02 17:03:55 +0100

    Added Termite Folder with scripts

---
+++ Termite-Cli/etc/platform/linux/env_setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#
+# Absolute path to the Termite CLI tool
+#
+export TERMITE_CLI_PATH=/home/jrsoares/Desktop/lab04-fixed/lab04/Termite-Cli-20190403/Termite-Cli
+
+#
+# Target platform; one of: mac, linux, or windows
+#
+export TERMITE_PLATFORM=linux
